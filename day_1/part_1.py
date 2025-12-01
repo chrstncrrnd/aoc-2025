1ce44d421d9c549f0e746820db339368ff3a6d2c
chrstncrrnd <chrstncrrnd@gmail.com>
2025-12-01 15:22:16 +0100

    day one

---
+++ day_1/part_1.py
@@ -0,0 +1,21 @@
+print("Day one advent of code 2025!")
+
+
+file = open("input.txt")
+
+puzzleInput = file.read().splitlines()
+pointing = 50
+password = 0
+
+for line in puzzleInput:
+    line = line.replace("R", "").replace("L", "-")
+    line = int(line)
+    pointing += line
+
+    pointing = pointing % 100
+    if pointing == 0:
+        password += 1
+
+
+print("Password is: ", password)
+
---
+++ day_1/part_2.py
@@ -0,0 +1,32 @@
+print("Day one advent of code 2025 (part two)!")
+
+
+file = open("input.txt")
+
+puzzleInput = file.read().splitlines()
+
+
+pointing = 50
+password = 0
+def timesCrossed(start, end):
+    if start == 0 and end < 0:
+        return abs(end // 100) - 1
+    if end == 0:
+        return 1
+    if end < 0 and end % 100 == 0:
+        return abs(end // 100) + 1
+    return abs(end // 100)
+
+
+
+
+for line in puzzleInput:
+    line = line.replace("R", "").replace("L", "-")
+    line = int(line)
+    password += timesCrossed(pointing, pointing + line)
+    pointing += line
+    pointing = pointing % 100
+
+
+print("Password is: ", password)
+
