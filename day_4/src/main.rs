e40e3ec005edf025b594a41b58651485cf37b3f2
chrstncrrnd <chrstncrrnd@gmail.com>
2025-12-05 10:56:32 +0100

    update

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -3,8 +3,8 @@ mod part_2;
 
 
 fn main() {
-    //println!("Day 4, part one!");
-    //part_1::main();
+    println!("Day 4, part one!");
+    part_1::main();
     println!("Day 4, part two!");
     part_2::main();
 }
