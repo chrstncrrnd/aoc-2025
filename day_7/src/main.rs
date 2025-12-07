e8b06d9b30c4e4bae5a9330477354b547d2f5914
chrstncrrnd <chrstncrrnd@gmail.com>
2025-12-07 15:15:53 +0100

    dat 7 part one

---
+++ day_7/src/main.rs
@@ -0,0 +1,7 @@
+mod part_1;
+
+
+fn main() {
+    println!("Day 7 part one!");
+    part_1::main();
+}
---
+++ day_7/src/part_1.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+pub fn main(){
+    let input = fs::read_to_string("input.txt").expect("No input file found!");
+    let lines: Vec<&str> = input.lines().collect();
+    let width = lines.first().unwrap().len();
+    let mut beams: Vec<usize> = vec![0; width];
+    beams[lines.first().unwrap().find("S").unwrap()] = 1;
+    let mut intersections = 0;
+
+
+    for line in lines{
+        for (i, c) in line.chars().enumerate(){
+            if c == '^' && beams[i] == 1{
+                if i > 0{
+                    beams[i - 1] = 1;
+                }
+                if i < beams.len() - 1{
+                    beams[i+1] = 1
+                }
+
+                beams[i] = 0;
+                intersections += 1;
+
+            }
+        }
+
+    }
+
+    println!("Total intersections: {}", intersections);
+
+}
